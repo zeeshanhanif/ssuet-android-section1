d3693b323e272795c904c8b33701123309006143
Samra <samra_nisarsid27@outlook.com>
2016-11-01 13:07:22 +0500

    class work of 23Oct 2016

---
+++ 23Oct2016/DemoList3/app/src/main/java/com/example/android/demolist/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.android.demolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Contact[] contacts;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fillContacts();
+
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contacts);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_item, R.id.contact,  contacts);
+        MyListAdapter adapter = new MyListAdapter(this,contacts);
+        ListView listView = (ListView) findViewById(R.id.contactList);
+        listView.setAdapter(adapter);
+        //R.layout.list_item
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,"Position = "+position+" == Value = "+contacts[position],Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void fillContacts(){
+        contacts = new Contact[50];
+        for(int i=0;i<contacts.length;i++){
+            int num = 1234567;
+            contacts[i] = new Contact("Contact "+(i+1),++num);
+        }
+    }
+}
